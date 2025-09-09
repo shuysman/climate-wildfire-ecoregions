e04f672119e50356f6a349f52827b4bfc255f739
Stephen Huysman <shuysman@gmail.com>
2025-09-09 17:15:14 -0600

    add wip forecast fire ignition danger map script

---
+++ src/map_forecast_danger.R
@@ -0,0 +1,153 @@
+### Proof of concept script illustrating how to map wildfire ignition
+### danger. Percentile of n-day rolling sum of VPD is estimated by
+### comparing rolling sums of VPD to precalculated quantiles of VPD.
+### This enables more rapid estimation of percentiles with low memory
+### requirements to allow for estimation across large areas which
+### would otherwise by memory-limited. Wildfire ignition danger is
+### represented as a value from 0-1, which are the historical
+### proportion of wildfires that burned at or below the corresponding
+### percentile of dryness.
+
+library(tidyverse)
+library(terra)
+library(tidyterra)
+library(glue)
+library(maptiles)
+library(climateR)
+
+replace_duplicated <- function(x) {
+  x[duplicated(x)] <- NA
+  return(x)
+}
+
+terraOptions(
+  verbose = TRUE,
+  memfrac = 0.9
+)
+
+out_dir <- file.path("out")
+
+## Optimal rolling windows determined by dryness analysis script
+
+probs <- seq(.01, 1.0, by = .01)
+
+nps_boundaries <- vect("data/nps_boundary/nps_boundary.shp") %>%
+  filter(UNIT_CODE %in% c("YELL", "GRTE", "JODR"))
+
+forest_quants <- rast("./out/ecdf/17-middle_rockies-forest/17-middle_rockies-forest-5-VPD-quants.nc")
+non_forest_quants <- rast("./out/ecdf/17-middle_rockies-forest/17-middle_rockies-forest-5-VPD-quants.nc")
+
+vpd_forecast <- rast("http://thredds.northwestknowledge.net:8080/thredds/fileServer/NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/cfsv2_metdata_90day/cfsv2_metdata_forecast_vpd_daily.nc")
+
+time(vpd_forecast) <- as_date(depth(vpd_forecast), origin = "1900-01-01")
+
+gridmet_url <- "http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_met_vpd_1979_CurrentYear_CONUS.nc?daily_mean_vapor_pressure_deficit[0:4:0][0:100:0][0:100:0]"
+vpd_gridmet <- rast(gridmet_url)
+
+today <- today()
+start_date <- today - 40
+
+vpd_gridmet <- getGridMET(
+  AOI = nps_boundaries,
+  varname = "vpd",
+  startDate = start_date,
+  endDate = today - 2,
+  verbose = TRUE
+)
+
+bin_rast <- function(new_rast, quants_rast, probs) {
+  # Count how many quantile layers the new value is greater than.
+  # This results in a raster of integers from 0 to 9.
+  bin_index_rast <- sum(new_rast > quants_rast)
+
+  # Now, map this integer index back to a percentile value.
+  # We need a mapping from [0, 1, 2, ..., 9] to [0, 0.1, 0.2, ..., 0.9]
+  # A value of 0 means it was smaller than the 1st quantile (q_0.1)
+  # A value of 9 means it was larger than the 9th quantile (q_0.9)
+  percentile_map <- c(0, probs)
+  from_vals <- 0:length(probs)
+  rcl_matrix <- cbind(from_vals, percentile_map)
+
+  # Use classify to create the final approximate percentile raster
+  percentile_rast_binned <- classify(bin_index_rast, rcl = rcl_matrix)
+
+  return(percentile_rast_binned)
+}
+
+
+forest_fire_danger_rast <- rast()
+for (n in 1:nlyr(forest_test)) {
+  forest_percentile_rast <- bin_rast(subset(forest_test, n), forest_quants_rast, probs)
+  forest_fire_danger_ecdf <- readRDS("/home/steve/sync/pyrome-fire/out/ecdf/17-middle_rockies-forest/17-middle_rockies-forest-5-VPD-ecdf.RDS")
+  forest_fire_danger_rast <- c(forest_fire_danger_rast, terra::app(forest_percentile_rast, fun = \(x) forest_fire_danger_ecdf(x)))
+}
+
+non_forest_fire_danger_rast <- rast()
+for (n in 1:nlyr(non_forest_test)) {
+  non_forest_percentile_rast <- bin_rast(subset(non_forest_test, n), non_forest_quants_rast, probs)
+  non_forest_fire_danger_ecdf <- readRDS("/home/steve/sync/pyrome-fire/out/ecdf/17-middle_rockies-non_forest/17-middle_rockies-non_forest-21-VPD-ecdf.RDS")
+  non_forest_fire_danger_rast <- c(non_forest_fire_danger_rast, terra::app(non_forest_percentile_rast, fun = \(x) non_forest_fire_danger_ecdf(x)))
+}
+
+cover_types <- rast("data/LF2023_EVT_240_CONUS/Tif/4326/LC23_EVT_240.tif") %>%
+  crop(forest_fire_danger_rast)
+
+activeCat(cover_types) <- "EVT_LF"
+
+# 1. Extract the category levels into a data frame
+# The [[1]] is used because levels() returns a list, one for each layer.
+categories_df <- levels(cover_types)[[1]]
+
+# 2. Add a new column with the desired classification using your rules
+# We will create numeric codes: 1 for non_forest, 2 for forest.
+# Everything else will become NA.
+categories_df <- categories_df %>%
+  mutate(veg_class_id = case_match(
+    EVT_LF,
+    c("Herb", "Shrub", "Sparse") ~ 1,
+    "Tree" ~ 2,
+    .default = NA_integer_
+  ))
+
+# 3. Create the reclassification matrix from the original ID to the new ID
+# The matrix should have two columns: 'from' (original ID) and 'to' (new ID).
+# The original raster values are in the 'ID' or 'value' column of the levels table.
+rcl_matrix <- categories_df[, c("Value", "veg_class_id")]
+
+# 4. Classify the raster using this matrix
+classified_rast <- classify(cover_types, rcl = rcl_matrix, right = NA)
+
+# 5. (Optional but recommended) Assign new category labels to the output raster
+new_levels <- data.frame(
+  ID = c(1, 2),
+  cover = c("non_forest", "forest")
+)
+levels(classified_rast) <- new_levels
+
+# View the result
+print(classified_rast)
+plot(classified_rast)
+
+forest_mask <- classified_rast == 2
+forest_mask <- subst(forest_mask, FALSE, NA)
+non_forest_mask <- classified_rast == 1
+non_forest_mask <- subst(non_forest_mask, FALSE, NA)
+
+basemap <- get_tiles(classified_rast, provider = "Esri.NatGeoWorldMap", zoom = 9) %>%
+  crop(classified_rast)
+
+forest_fire_danger_rast <- forest_fire_danger_rast %>% resample(classified_rast)
+non_forest_fire_danger_rast <- non_forest_fire_danger_rast %>% resample(classified_rast)
+
+names(forest_fire_danger_rast) <- test_dates
+names(non_forest_fire_danger_rast) <- test_dates
+
+ggplot() +
+  geom_spatraster_rgb(data = basemap) +
+  geom_spatraster(data = mask(forest_fire_danger_rast, forest_mask)) +
+  # scale_fill_viridis_c(option = "B", na.value = "transparent", limits = c(0, 1)) +
+  # ggnewscale::new_scale_fill() +
+  geom_spatraster(data = mask(non_forest_fire_danger_rast, non_forest_mask)) +
+  scale_fill_viridis_c(option = "B", na.value = "transparent", limits = c(0, 1)) +
+  facet_wrap(~lyr)
+# ggsave("fire_danger_example.png", height = 8, width = 10)
